7b3ccf82969918fe03c7dbf11f06876169216795
joethorpe6 <joethorpe6@openmailbox.org>
2017-10-18 09:13:34 +0100

    another small bug fix

--- other_scans.sh
+++ other_scans.sh
@@ -86,10 +86,10 @@ scan(){
 	for ip in $(cat open-ports/443.txt)
 	do
 	    curl -v https://$ip/ -H "Host: hostname" \
-		 -H "Range: bytes=0-18446744073709551615" -k >> nse_scans/MS15034/$ip.txt
+		 -H "Range: bytes=0-18446744073709551615" -k >> nse_scans/MS15034-$ip.txt
 	done
     fi            
 }
 
 #-- call function
-scans
+scan
